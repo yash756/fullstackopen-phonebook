14d99104c2af7bb7f3ea737b0a8e5285925bf770
yash756 <penteyash@gmail.com>
2023-08-15 23:25:18 +0530

    step 6 done : fetched data from server using axios

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-    ]);
+    const [persons, setPersons] = useState([]);
+    // { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    // { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    // { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    // { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
 
-    let currentId = persons[persons.length - 1].id;
+    //let currentId = persons[persons.length - 1].id;
 
     const [newName, setNewName] = useState('');
     const [newNumber, setNewNumber] = useState('');
     const [filter, setFilter] = useState('');
+
+    // fetching data from a server
+    useEffect(() => {
+        axios.get('http://localhost:3001/persons').then((response) => {
+            console.log('response', response.data);
+            setPersons(response.data);
+        });
+    }, []);
 
     const addPerson = (event) => {
         event.preventDefault();
@@ -22,7 +30,7 @@ const App = () => {
         const personObject = {
             name: newName,
             number: newNumber,
-            id: currentId + 1,
+            id: persons[persons.length - 1].id + 1,
         };
 
         let exists = false;
