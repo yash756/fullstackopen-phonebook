c67225c45562434886eb66419f2a7445bc3cd1d8
yash756 <penteyash@gmail.com>
2023-08-20 22:59:07 +0530

    step 7 done : added axios post

--- src/App.js
+++ src/App.js
@@ -3,98 +3,99 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const App = () => {
-    const [persons, setPersons] = useState([]);
-    // { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    // { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    // { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    // { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+	const [persons, setPersons] = useState([]);
+	const [newName, setNewName] = useState('');
+	const [newNumber, setNewNumber] = useState('');
+	const [filter, setFilter] = useState('');
 
-    //let currentId = persons[persons.length - 1].id;
+	// fetching data from a server
+	useEffect(() => {
+		axios.get('http://localhost:3001/persons').then((response) => {
+			console.log('response', response.data);
+			setPersons(response.data);
+		});
+	}, []);
 
-    const [newName, setNewName] = useState('');
-    const [newNumber, setNewNumber] = useState('');
-    const [filter, setFilter] = useState('');
+	const addPerson = (event) => {
+		event.preventDefault();
 
-    // fetching data from a server
-    useEffect(() => {
-        axios.get('http://localhost:3001/persons').then((response) => {
-            console.log('response', response.data);
-            setPersons(response.data);
-        });
-    }, []);
+		const personObject = {
+			name: newName,
+			number: newNumber,
+			id: persons[persons.length - 1].id + 1,
+		};
 
-    const addPerson = (event) => {
-        event.preventDefault();
-        //console.log('button clicked', event.target);
+		let exists = false;
+		persons.forEach((person) => {
+			if (
+				person.name === personObject.name &&
+				person.number === personObject.number
+			) {
+				exists = true;
+				console.log(exists);
+				return alert(
+					`${personObject.name} is already added to the phonebook`
+				);
+			}
+		});
 
-        const personObject = {
-            name: newName,
-            number: newNumber,
-            id: persons[persons.length - 1].id + 1,
-        };
+		if (!exists) {
+			axios
+				.post('http://localhost:3001/persons', personObject)
+				.then((response) => {
+					console.log(response);
+					setPersons(persons.concat(personObject));
+					setNewName('');
+					setNewNumber('');
+				});
+		}
+	};
 
-        let exists = false;
-        persons.forEach((person) => {
-            if (JSON.stringify(person) === JSON.stringify(personObject)) {
-                exists = true;
-                return alert(
-                    `${personObject.name} is already added to the phonebook`
-                );
-            }
-        });
+	const handleNameChange = (event) => {
+		setNewName(event.target.value);
+	};
 
-        if (!exists) {
-            setPersons(persons.concat(personObject));
-            setNewName('');
-            setNewNumber('');
-        }
-    };
+	const handleNumberChange = (event) => {
+		setNewNumber(event.target.value);
+	};
 
-    const handleNameChange = (event) => {
-        setNewName(event.target.value);
-    };
+	const handleFilter = (event) => {
+		setFilter(event.target.value);
+	};
 
-    const handleNumberChange = (event) => {
-        setNewNumber(event.target.value);
-    };
+	const peopleToShow = persons.filter(
+		(person) =>
+			person.name.includes(filter) ||
+			person.name.includes(filter.toUpperCase())
+	);
 
-    const handleFilter = (event) => {
-        setFilter(event.target.value);
-    };
-
-    const peopleToShow = persons.filter(
-        (person) =>
-            person.name.includes(filter) ||
-            person.name.includes(filter.toUpperCase())
-    );
-
-    return (
-        <div>
-            <h2>Phonebook</h2>
-            <div>
-                filter: <input value={filter} onChange={handleFilter} />
-            </div>
-            <h2>Add a number</h2>
-            <form onSubmit={addPerson}>
-                <div>
-                    name: <input value={newName} onChange={handleNameChange} />
-                </div>
-                <div>
-                    number:{' '}
-                    <input value={newNumber} onChange={handleNumberChange} />
-                </div>
-                <div>
-                    <button type="submit">add</button>
-                </div>
-            </form>
-            <h2>Numbers</h2>
-            {peopleToShow.map((person) => (
-                <div key={person.id}>
-                    {person.name} {person.number}
-                </div>
-            ))}
-        </div>
-    );
+	return (
+		<div>
+			<h2>Phonebook</h2>
+			<div>
+				filter: <input value={filter} onChange={handleFilter} />
+			</div>
+			<h2>Add a number</h2>
+			<form onSubmit={addPerson}>
+				<div>
+					name: <input value={newName} onChange={handleNameChange} />
+				</div>
+				<div>
+					number:{' '}
+					<input value={newNumber} onChange={handleNumberChange} />
+				</div>
+				<div>
+					<button type="submit">add</button>
+				</div>
+			</form>
+			<h2>Numbers</h2>
+			{peopleToShow.map((person) => (
+				<div key={person.id}>
+					{person.name} {person.number}
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default App;
